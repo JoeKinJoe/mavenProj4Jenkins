9298411cb4db53d1a0edcbaaacaa5026185a1e4d
majortomdev <kinahanj25@gmail.com>
2018-01-23 01:02:06 +0000

    this is with--out---sigh.... the extra variable

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-      private String thiskind = "this is me on monday building";
+      //private String thiskind = "this is me on monday building";
     return String.format("Hello "+thiskind+" you fucking shitbags, %s!", someone);
   }
 }
