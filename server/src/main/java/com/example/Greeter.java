9d092741809238a0efee532354703afc181807c5
majortomdev <kinahanj25@gmail.com>
2018-01-23 16:42:27 +0000

    with more......

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,9 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  
   public final String greet(final String someone) {
-      //private String thiskind = "this is me on monday building";
+      
     return String.format("Hello u fucking shitbags, %s!", someone);
   }
 }
