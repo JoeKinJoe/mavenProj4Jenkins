e63d5ea9f581e51074ab1e066bb48867a36d8080
majortomdev <kinahanj25@gmail.com>
2018-01-23 16:26:57 +0000

    the execution

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,6 +15,6 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
       //private String thiskind = "this is me on monday building";
-    return String.format("Hello............ you fucking shitbags, %s!", someone);
+    return String.format("Hello u fucking shitbags, %s!", someone);
   }
 }
